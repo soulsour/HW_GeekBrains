023b5be7a6d968647dcb09fab0cbce27f9d57481
Andrew Olschansky <soulsour@me.com>
2018-03-19 18:19:14 +0300

    Little Changes

--- DBEditor/DBEditor/ChildWindow.xaml.cs
+++ DBEditor/DBEditor/ChildWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Windows;
 
 
@@ -11,45 +12,34 @@ namespace DBEditor
     public partial class ChildWindow : Window
     {
 
-        Employee TheWorker;
-        int index;
-
-        public ChildWindow(ObservableCollection<Department> deps, ObservableCollection<Employee> emps, int _index)
+     
+        public DataRow resultRow { get; set; }
+        public ChildWindow(DataRow dataRow)
         {
             InitializeComponent();
-            index = _index;
-            TheWorker = emps[index];                   // узнаём, с каким элементом списка работаем
-
-            comboBoxDep.ItemsSource = deps;            // заполняем окошко данными элемента списка
-            textBoxid.Text = TheWorker.Id.ToString();
-            textBoxAge.Text = TheWorker.Age.ToString();
-            textBoxName.Text = TheWorker.Name;
-            textBoxSalary.Text = TheWorker.Salary.ToString();
-            comboBoxDep.Text = TheWorker.Department == "Empty" ? deps[0].Name : TheWorker.Department;
+            resultRow = dataRow;
         }
 
         private void buttonSave_Click(object sender, RoutedEventArgs e)
         {
-            TheWorker.Name = textBoxName.Text;
-            TheWorker.Department = comboBoxDep.Text;
-            try
-            {
-                TheWorker.Id = Int32.Parse(textBoxid.Text);
-                TheWorker.Age = Int32.Parse(textBoxAge.Text);
-                TheWorker.Salary = Double.Parse(textBoxSalary.Text);
-            }
-            catch (Exception exc)
-            {
-                MessageBox.Show(exc.Message);
-            }
-            finally
-            {
-                MainWindow main = this.Owner as MainWindow;
-                main.Update(TheWorker, index);
-                this.Close();
-            }
+            resultRow["ID"] = textBoxid.Text;
+            resultRow["Name"] = textBoxName.Text;
+            resultRow["Age"] = textBoxAge.Text;
+            resultRow["Salary"] = textBoxSalary.Text;
+            this.DialogResult = true;
 
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+            textBoxName.Text = resultRow["Name"].ToString();
+            textBoxAge.Text = resultRow["Age"].ToString();
+            textBoxSalary.Text = resultRow["Salary"].ToString();
+
+        }
+
+       
     }
 }
 
